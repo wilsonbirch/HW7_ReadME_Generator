4998837b1aca95e979f82758fe1b3fb02654dbd0
Wilson Birch <wilsonbirch@gmail.com>
2020-07-17 14:49:31 -0400

    installed inquirer and committing package-lock.json

--- index.js
+++ index.js
@@ -5,10 +5,29 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+    
 }
 
 // function to initialize program
 function init() {
+
+    let inquirer = require('inquirer');
+
+    inquirer.prompt([
+        `What is the title of your README?`
+    ])
+    .then( titleAnswer => {
+        console.log(titleAnswer);
+    })
+    .catch (error => {
+
+        if(error.isTtyError) {
+            //
+        } else {
+            //
+        }
+    });
 
 }
 
